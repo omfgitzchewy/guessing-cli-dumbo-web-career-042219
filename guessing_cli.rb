4a27f05829cbabea947a65506d5d054331d3d262
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 18:36:12 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     input = gets.chomp.to_s
     if input == random_num.to_s
       puts "You guessed the correct number!"
-    else
+    else input != random_num.to_s
       puts "The computer guessed #{random_num}"
     end
   end
